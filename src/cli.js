6cfc9d0e115c42b90335737a0b9665ef93f9c603
Darya <meow.daria@inbox.ru>
2024-05-22 09:10:51 +0000

    cli

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export function User() {
+export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}`);
   return name;
-}
-
-export const lose = (answer, correctAnswer, name) => {
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  console.log(`Let's try again, ${name}!`);
 };
 
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
