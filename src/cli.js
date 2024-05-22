c8f78630706b074cd57d4cedb86aac4df87f1a03
Darya <meow.daria@inbox.ru>
2024-05-22 08:53:24 +0000

    cli

--- src/cli.js
+++ src/cli.js
@@ -5,5 +5,11 @@ export function User() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
+}
+
+export const lose = (answer, correctAnswer, name) => {
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  console.log(`Let's try again, ${name}!`);
 };
 
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
